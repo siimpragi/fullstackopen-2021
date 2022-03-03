f27a0247c10b213a5c784fa316e7bbbcc0793298
Siim Pragi <siim@pragi.ee>
2022-03-03 23:49:18 +0200

    Solve ex 6-11

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import Button from "./components/Button";
+import Statistics from "./components/Statistics";
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
---
+++ part1/unicafe/src/components/Button.js
@@ -0,0 +1,5 @@
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+export default Button;
