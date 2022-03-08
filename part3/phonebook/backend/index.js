ce392af934e11ab6cdc0f52912fe34f6b280d5f0
Siim Pragi <siim@pragi.ee>
2022-03-08 05:23:46 +0200

    Solve ex 13-14

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
+const app = express();
+const Person = require("./models/person");
 const morgan = require("morgan");
 const cors = require("cors");
-
-const app = express();
 
 app.use(cors());
 app.use(express.static("build"));
@@ -19,52 +20,28 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
-  return maxId + 1;
-};
-
 app.get("/info", (_, res) => {
-  const info = `<p>Phonebook has info for ${
-    persons.length
-  } people.</p><p>${new Date()}</p>`;
-  res.send(info);
+  Person.find({}).then((persons) => {
+    const info = `<p>Phonebook has info for ${
+      persons.length
+    } people.</p><p>${new Date()}</p>`;
+    res.send(info);
+  });
 });
 
 app.get("/api/persons", (_, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((p) => p.id === id);
-  if (!person) {
-    return res.status(404).json({ error: "no such person" });
-  }
-  res.json(person);
+  Person.findById(req.params.id).then((person) => {
+    if (!person) {
+      return res.status(404).json({ error: "no such person" });
+    }
+    res.json(person);
+  });
 });
 
 app.post("/api/persons", (req, res) => {
@@ -76,26 +53,13 @@ app.post("/api/persons", (req, res) => {
   if (!number) {
     return res.status(400).json({ error: "'number' is missing" });
   }
-  const personWithMatchingName = persons.find((p) => p.name === name);
-  if (personWithMatchingName) {
-    return res.status(400).json({ error: "'name' must be unique" });
-  }
-
-  const person = {
-    id: generateId(),
-    name,
-    number,
-  };
-  persons = persons.concat(person);
-  res.json(person);
+  const person = new Person({ name, number });
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
 app.put("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((p) => p.id === id);
-  if (!person) {
-    return res.status(404).json({ error: "no such person" });
-  }
   const { name, number } = req.body;
   if (!name) {
     return res.status(400).json({ error: "'name' is missing" });
@@ -103,26 +67,21 @@ app.put("/api/persons/:id", (req, res) => {
   if (!number) {
     return res.status(400).json({ error: "'number' is missing" });
   }
-  if (person.name !== name) {
-    const personWithMatchingName = persons.find((p) => p.name === name);
-    if (personWithMatchingName) {
-      return res.status(400).json({ error: "'name' must be unique" });
+  // TODO: not sure if 'Update' is correct in case of a PUT
+  Person.findByIdAndUpdate(req.params.id, { name, number }).then(
+    (updatedPerson) => {
+      if (!updatedPerson) {
+        return res.status(404).json({ error: "no such person" });
+      }
+      res.json(updatedPerson);
     }
-  }
-
-  const updatedPerson = {
-    id,
-    name,
-    number,
-  };
-  persons = persons.map((p) => (p.id === id ? updatedPerson : p));
-  res.json(updatedPerson);
+  );
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((p) => p.id !== id);
-  res.status(204).end();
+  Person.findByIdAndRemove(req.params.id).then(() => {
+    res.status(204).end();
+  });
 });
 
 const unknownEndpoint = (_, res) => {
@@ -130,7 +89,7 @@ const unknownEndpoint = (_, res) => {
 };
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.`);
 });
